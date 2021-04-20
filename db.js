0dfe033256cc886ed9f9a78ee6fee396d9f6b2f0
Alexander Alie <alexanderalie@alexanders-mbp.home>
2021-04-20 12:29:47 -0400

    reupload

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql');
-const databaseString =  "mysql://root:11091991@localhost:3306/first_sql"
+const databaseString =  "mysql://root:@localhost:3306/first_sql"
 const databaseConnection = mysql.createConnection(databaseString);
 
 
